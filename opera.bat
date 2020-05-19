e5840c60c88eccd0c063d3449d9d84770754cd4a
Heavenanvil <heavenanvilz@gmail.com>
2020-05-19 11:10:08 +0700

    Версия 1.2
    Добавлены условия поиска Оперы в трёх разных папках.
    Добавлено описание проекта.

--- opera.bat
+++ opera.bat
@@ -1,36 +1,7 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFKuF/Noy7VeeA6YX/Ofr0+KItl0TU6w8d4KV07eBQA==
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSzk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFKuF/Noy7VeeA6YX/Ofr08W3h388ArZxfZfeug==
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+:: © Heavenanvil
+:: E-mail: heavenanvil@gmail.com
+:: version 1.2
+
 @echo off
 tasklist | find /i "opera.exe" >NUL && (
     taskkill /im "opera.exe" /f
@@ -44,4 +15,16 @@ tasklist | find /i "opera_crashreporter.exe" >NUL && (
 tasklist | find /i "launcher.exe" >NUL && (
     taskkill /im "launcher.exe" /f
 )
-start opera.exe
+if exist "%HomeDrive%\Program Files\Opera\launcher.exe" (
+	start "" "%HomeDrive%\Program Files\Opera\launcher.exe"
+) else (
+	if exist "%HomeDrive%\Program Files (x86)\Opera\launcher.exe" (
+		start "" "%HomeDrive%\Program Files (x86)\Opera\launcher.exe"
+	) else (
+		if exist "%LocalAppData%\Opera\launcher.exe" (
+			start "" "%LocalAppData%\Opera\launcher.exe"
+		) else (
+			start "" opera.exe
+		)
+	)
+)
